d20ac1e44ccf3b8b15883de29d77859478f0c0ec
mste <m.stein@conterra.de>
2019-03-19 08:53:25 +0100

    Reformat WelcomeWidget

--- src/main/js/bundles/dn_welcome/WelcomeWidget.vue
+++ src/main/js/bundles/dn_welcome/WelcomeWidget.vue
@@ -1,49 +1,62 @@
 <template>
     <div class="welcome">
         <div class="top">
-            <v-toolbar class="primary title" dense>
-                <v-toolbar-title>{{title}}</v-toolbar-title>
+            <v-toolbar
+                class="primary title"
+                dense>
+                <v-toolbar-title>{{ title }}</v-toolbar-title>
             </v-toolbar>
         </div>
         <div class="center">
-            <v-container grid-list-md class="pa-1">
-                <v-img v-if="imgUrl"
-                       v-bind:height="imgHeight"
-                       v-bind:src="imgUrl">
-                </v-img>
-                <v-card-title v-html="infoText">
-                </v-card-title>
+            <v-container
+                grid-list-md
+                class="pa-1">
+                <v-img
+                    v-if="imgUrl"
+                    :height="imgHeight"
+                    :src="imgUrl"/>
+                <v-card-title v-html="infoText"/>
             </v-container>
         </div>
         <div class="bottom">
-            <v-container v-if="accept" grid-list-md text-xs-center class="pa-1">
-                <v-checkbox v-if="accept"
-                            v-bind:label="checkboxText"
-                            v-model="checkBox"
-                            class="pa-2"
-                            color="primary"
-                            hide-details>
-                </v-checkbox>
-                <v-btn v-on:click="$emit('close')"
-                       v-bind:disabled="!checkBox"
-                       ripple
-                       color="primary">
-                    {{buttonText}}
+            <v-container
+                v-if="accept"
+                grid-list-md
+                text-xs-center
+                class="pa-1">
+                <v-checkbox
+                    v-if="accept"
+                    :label="checkboxText"
+                    v-model="checkBox"
+                    class="pa-2"
+                    color="primary"
+                    hide-details/>
+                <v-btn
+                    :disabled="!checkBox"
+                    ripple
+                    color="primary"
+                    @click="$emit('close')">
+                    {{ buttonText }}
                 </v-btn>
             </v-container>
-            <v-container v-else grid-list-md text-xs-center class="pa-1">
-                <v-btn v-on:click="$emit('close')"
-                       ripple
-                       color="primary">
-                    {{buttonText}}
+            <v-container
+                v-else
+                grid-list-md
+                text-xs-center
+                class="pa-1">
+                <v-btn
+                    ripple
+                    color="primary"
+                    @click="$emit('close')">
+                    {{ buttonText }}
                 </v-btn>
-                <v-checkbox v-if="!accept"
-                            v-bind:label="checkboxText"
-                            v-model="checkBox"
-                            class="pa-2"
-                            color="primary"
-                            hide-details>
-                </v-checkbox>
+                <v-checkbox
+                    v-if="!accept"
+                    :label="checkboxText"
+                    v-model="checkBox"
+                    class="pa-2"
+                    color="primary"
+                    hide-details/>
             </v-container>
         </div>
     </div>
@@ -64,8 +77,6 @@
                 imgUrl: "",
                 imgHeight: "150px"
             };
-        },
-        methods: {}
-
+        }
     };
 </script>
