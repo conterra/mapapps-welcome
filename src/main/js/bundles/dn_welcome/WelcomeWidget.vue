7147a605b001bdcdf6c47b9ba2bf706105f43f77
Dennis Payk - con terra GmbH <d.payk@conterra.de>
2022-05-12 11:44:29 +0200

    Use vuetify card style

--- src/main/js/bundles/dn_welcome/WelcomeWidget.vue
+++ src/main/js/bundles/dn_welcome/WelcomeWidget.vue
@@ -17,79 +17,75 @@
 -->
 
 <template>
-    <div class="welcome">
-        <div class="top">
-            <v-toolbar
-                class="primary title"
-                dense
-            >
-                <v-toolbar-title>{{ title }}</v-toolbar-title>
-            </v-toolbar>
-        </div>
-        <div class="center">
-            <v-container
-                grid-list-md
-                class="pa-1"
-            >
-                <v-img
-                    v-if="imgUrl"
-                    :height="imgHeight"
-                    :src="imgUrl"
-                />
-                <v-card-title>{{ infoText }}</v-card-title>
-            </v-container>
-        </div>
-        <div class="bottom">
-            <v-container
-                v-if="accept"
-                grid-list-md
-                text-xs-center
-                class="pa-1"
-            >
-                <v-checkbox
-                    v-if="accept"
-                    v-model="checkBox"
-                    :label="checkboxText"
-                    class="pa-2"
-                    color="primary"
-                    hide-details
-                />
-                <v-btn
-                    :disabled="!checkBox"
-                    ripple
-                    color="primary"
-                    @click="$emit('close')"
-                >
-                    {{ buttonText }}
-                </v-btn>
-            </v-container>
-            <v-container
-                v-else
-                grid-list-md
-                text-xs-center
-                class="pa-1"
-            >
-                <v-btn
-                    ripple
-                    color="primary"
-                    @click="$emit('close')"
-                >
-                    {{ buttonText }}
-                </v-btn>
-                <v-checkbox
-                    v-if="!accept"
-                    v-model="checkBox"
-                    :label="checkboxText"
-                    class="pa-2"
-                    color="primary"
-                    hide-details
-                />
-            </v-container>
-        </div>
-    </div>
-</template>
-<script>
+    <v-layout>
+        <v-card>
+            <v-img
+                v-if="imgUrl"
+                :height="imgHeight"
+                :src="imgUrl"
+            />
 
+            <v-card-title primary-title>
+                <div>
+                    <h3 class="headline mb-0">
+                        {{ title }}
+                    </h3>
+                    <div> {{ infoText }} </div>
+                </div>
+            </v-card-title>
+
+            <v-card-actions>
+                <v-container
+                    v-if="accept"
+                    grid-list-md
+                    text-xs-center
+                    class="pa-1"
+                >
+                    <v-checkbox
+                        v-if="accept"
+                        v-model="checkBox"
+                        :label="checkboxText"
+                        class="pa-2"
+                        color="primary"
+                        hide-details
+                    />
+                    <v-btn
+                        :disabled="!checkBox"
+                        ripple
+                        color="primary"
+                        @click="$emit('close')"
+                    >
+                        {{ buttonText }}
+                    </v-btn>
+                </v-container>
+                <v-container
+                    v-else
+                    grid-list-md
+                    text-xs-center
+                    class="pa-1"
+                >
+                    <v-btn
+                        ripple
+                        color="primary"
+                        @click="$emit('close')"
+                    >
+                        {{ buttonText }}
+                    </v-btn>
+                    <v-checkbox
+                        v-if="!accept"
+                        v-model="checkBox"
+                        :label="checkboxText"
+                        class="pa-2"
+                        color="primary"
+                        hide-details
+                    />
+                </v-container>
+            </v-card-actions>
+        </v-card>
+    </v-layout>
+</template>
+
+<script>
     export default {
         data: function () {
             return {
