f8f54757230eb05472c3cf1bd2dad75725af3e00
Marius Austerschulte <m.austerschulte@conterra.de>
2024-04-17 16:17:33 +0200

    Enabled rollup for dn_welcome bundle.

--- src/main/js/bundles/dn_welcome/build.config.js
+++ src/main/js/bundles/dn_welcome/build.config.js
@@ -13,4 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import "dojo/i18n!./nls/bundle";
+module.exports = {
+    type: "bundle",
+    entryPoints: ["./module"]
+};
