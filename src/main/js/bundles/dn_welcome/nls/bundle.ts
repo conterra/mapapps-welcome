b17465486be8ad34f60677ec216033b525f570bf
Marius Austerschulte <m.austerschulte@conterra.de>
2024-04-24 09:25:01 +0200

    DN-26: Migrated bundle.js files in dn_welcome bundle to TypeScript

---
+++ src/main/js/bundles/dn_welcome/nls/bundle.ts
@@ -0,0 +1,30 @@
+///
+/// Copyright (C) 2024 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+const i18n = {
+    root: ({
+        bundleName: "Welcome",
+        bundleDescription: "The Welcome Bundle is a widget for displaying a welcome screen with a Do-Not-Show-Again option",
+        tool: {
+            title: "Welcome Info",
+            tooltip: "Welcome Info"
+        }
+    }),
+    "de": true
+};
+
+export type Messages = (typeof i18n)["root"];
+export default i18n;
---
+++ src/main/js/bundles/dn_welcome/nls/de/bundle.ts
@@ -0,0 +1,28 @@
+///
+/// Copyright (C) 2024 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+import { type Messages } from "../bundle";
+
+const i18n = {
+    bundleName: "Welcome",
+    bundleDescription: "Das Welcome Bundle ist ein Widget mit dem man ein Startfenster konfigurieren kann.",
+    tool: {
+        title: "Welcome Info",
+        tooltip: "Welcome Info"
+    }
+} satisfies Messages;
+
+export default i18n;
